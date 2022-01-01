ac2fb9116b12c59a7a75367da3c9578c30db81ed
Seoyun0208 <sylph0105@naver.com>
2022-01-01 18:45:10 +0900

    6주차_객체지향 프로그래밍_자바와 자료구조(2-3)

---
+++ Chapter5/src/ch04/MyArrayStackTest.java
@@ -0,0 +1,22 @@
+package ch04;
+
+public class MyArrayStackTest {
+
+	public static void main(String[] args) {
+
+		MyArrayStack stack = new MyArrayStack(3);
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		stack.push(40);
+		System.out.println("=================");
+		stack.printAll();
+		System.out.println("=================");
+		System.out.println(stack.pop());
+		System.out.println("=================");
+		System.out.println(stack.pop());
+		System.out.println("=================");
+		System.out.println(stack.peek());
+	}
+
+}
